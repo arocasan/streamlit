accf1cccc856de24fd4cf077f49a22d072d32d83
arocasan <mr@michelantonio.com>
2023-05-05 23:59:35 +0200

    will model show up?

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import mymodel as m
 
 st.title("What is this!??!")
+
+windows = st.slider("Slide this to the right")
+st.write(m.run(window=window))
+
